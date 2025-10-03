2dd4bdbe190057b5b3077aac1a2df63ac31a6189
juampimedina06 <jpmedinagomez1@gmail.com>
2025-10-03 00:29:40 -0300

    creando estructura

---
+++ middleware/logging.js
@@ -0,0 +1,8 @@
+
+const loggingMdw = (req, res, next) =>{ 
+    console.log(`Se hizo un request a la URL: ${req.url}`)
+    res.header('Context-type', 'application/json')
+    next();
+}
+
+module.exports = loggingMdw;
